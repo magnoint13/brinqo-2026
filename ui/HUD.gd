e7f658fe2e5880b3898ffc8b866cd016c8bb7d7e
Garcigonz <ivanivan.ingagl@gmail.com>
2026-02-08 18:19:48 +0100

    Added Pause Menu

--- ui/HUD.gd
+++ ui/HUD.gd
@@ -5,20 +5,54 @@ extends CanvasLayer
 @onready var restart_button = $UI/RestartButton
 @onready var menu_button = $UI/MenuButton
 @onready var progress_bar = $UI/CollapseProgressBar
+@onready var pause_menu = $UI/PauseMenu
+@onready var resume_button = $UI/PauseMenu/CenterContainer/VBoxContainer/ResumeButton
+@onready var pause_restart_button = $UI/PauseMenu/CenterContainer/VBoxContainer/RestartButton2
+@onready var level_select_button = $UI/PauseMenu/CenterContainer/VBoxContainer/LevelSelectButton
+@onready var main_menu_button = $UI/PauseMenu/CenterContainer/VBoxContainer/MainMenuButton
 
 var max_collapse_time: float = 15.0
+var is_paused: bool = false
 
 func _ready():
 	restart_button.pressed.connect(_on_restart_pressed)
 	menu_button.pressed.connect(_on_menu_pressed)
+	resume_button.pressed.connect(_on_resume_pressed)
+	pause_restart_button.pressed.connect(_on_pause_restart_pressed)
+	level_select_button.pressed.connect(_on_level_select_pressed)
+	main_menu_button.pressed.connect(_on_main_menu_pressed)
 	# Style the progress bar
 	progress_bar.modulate = Color(1, 0.42, 0.42, 1)  # Reddish color matching timer
+
+func _input(event):
+	if event.is_action_pressed("ui_cancel"):
+		toggle_pause()
+
+func toggle_pause():
+	is_paused = !is_paused
+	pause_menu.visible = is_paused
+	get_tree().paused = is_paused
 
 func _on_restart_pressed():
 	GameManager.restart_current_level()
 
 func _on_menu_pressed():
+	toggle_pause()
+
+func _on_resume_pressed():
+	toggle_pause()
+
+func _on_pause_restart_pressed():
+	get_tree().paused = false
+	GameManager.restart_current_level()
+
+func _on_level_select_pressed():
+	get_tree().paused = false
 	GameManager.go_to_level_select()
+
+func _on_main_menu_pressed():
+	get_tree().paused = false
+	GameManager.go_to_main_menu()
 
 func set_timer_text(text: String):
 	timer_label.text = text
